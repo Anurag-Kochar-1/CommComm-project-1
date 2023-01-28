4f4f3c4b0902a11e0a4d97e752cc827a08786232
Anurag <anuragmarketing101@gmail.com>
2023-01-29 00:40:34 +0530

    demoUserI-added

---
+++ utils/adminSignIn/demoAccount.ts
@@ -0,0 +1,10 @@
+export interface demoAccount {
+    username: string | number
+    email: string
+    password: string | number
+    isAdmin: boolean
+    isAuthenticated: boolean
+    userDisplayPicture: string
+    displayName: string
+    isCommAccess: boolean
+}
