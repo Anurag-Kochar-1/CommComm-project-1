890ecaf15e3dcac12dfa545571828fcaee4b814a
Anurag <anuragmarketing101@gmail.com>
2023-01-29 17:30:18 +0530

    demoUsers-added

---
+++ components/globalComponents/Buttons/SignInAndSignUp/DemoAccountSignIn/DemoAccountSignIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DemoAccountSignIn = () => {
+  return (
+    <div>
+        <button title='demo' type='button' className='px-7 py-1 rounded-md' onClick={() => {
+            alert(`Email ->  Password ->  `)
+        }}> Demo Account </button>
+    </div>
+  )
+}
+
+export default DemoAccountSignIn
